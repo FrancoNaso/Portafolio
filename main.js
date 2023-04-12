2c81c829205d753bfafe21e064d05244e02da0ca
Franco Naso <francogabrielnaso@gmail.com>
2023-04-12 04:23:40 -0300

    hero

--- main.js
+++ main.js
@@ -1,69 +1,91 @@
 
 
-(() =>{
- 
+(() => {
+
     const openNavMenu = document.querySelector(".open-nav-menu"),
-    closeNavMenu = document.querySelector(".close-nav-menu"),
-    navMenu = document.querySelector(".nav-menu"),
-    menuOverlay = document.querySelector(".menu-overlay"),
-    mediaSize = 991;
-  
+        closeNavMenu = document.querySelector(".close-nav-menu"),
+        navMenu = document.querySelector(".nav-menu"),
+        menuOverlay = document.querySelector(".menu-overlay"),
+        mediaSize = 991;
+
     openNavMenu.addEventListener("click", toggleNav);
     closeNavMenu.addEventListener("click", toggleNav);
-    // close the navMenu by clicking outside
     menuOverlay.addEventListener("click", toggleNav);
-  
+
     function toggleNav() {
         navMenu.classList.toggle("open");
         menuOverlay.classList.toggle("active");
         document.body.classList.toggle("hidden-scrolling");
     }
-  
-    navMenu.addEventListener("click", (event) =>{
-        if(event.target.hasAttribute("data-toggle") && 
-            window.innerWidth <= mediaSize){
-            // prevent default anchor click behavior
+
+    navMenu.addEventListener("click", (event) => {
+        if (event.target.hasAttribute("data-toggle") &&
+            window.innerWidth <= mediaSize) {
             event.preventDefault();
             const menuItemHasChildren = event.target.parentElement;
-          // if menuItemHasChildren is already expanded, collapse it
-          if(menuItemHasChildren.classList.contains("active")){
-              collapseSubMenu();
-          }
-          else{
-            // collapse existing expanded menuItemHasChildren
-            if(navMenu.querySelector(".menu-item-has-children.active")){
-              collapseSubMenu();
+            if (menuItemHasChildren.classList.contains("active")) {
+                collapseSubMenu();
             }
-            // expand new menuItemHasChildren
-            menuItemHasChildren.classList.add("active");
-            const subMenu = menuItemHasChildren.querySelector(".sub-menu");
-            subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-          }
+            else {
+                if (navMenu.querySelector(".menu-item-has-children.active")) {
+                    collapseSubMenu();
+                }
+                menuItemHasChildren.classList.add("active");
+                const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+                subMenu.style.maxHeight = subMenu.scrollHeight + "px";
+            }
         }
     });
-    function collapseSubMenu(){
+    function collapseSubMenu() {
         navMenu.querySelector(".menu-item-has-children.active .sub-menu")
-        .removeAttribute("style");
+            .removeAttribute("style");
         navMenu.querySelector(".menu-item-has-children.active")
-        .classList.remove("active");
+            .classList.remove("active");
     }
-    function resizeFix(){
-         // if navMenu is open ,close it
-         if(navMenu.classList.contains("open")){
-             toggleNav();
-         }
-         // if menuItemHasChildren is expanded , collapse it
-         if(navMenu.querySelector(".menu-item-has-children.active")){
-              collapseSubMenu();
-       }
+    function resizeFix() {
+        if (navMenu.classList.contains("open")) {
+            toggleNav();
+        }
+        if (navMenu.querySelector(".menu-item-has-children.active")) {
+            collapseSubMenu();
+        }
     }
-  
-    window.addEventListener("resize", function(){
-       if(this.innerWidth > mediaSize){
-           resizeFix();
-       }
+
+    window.addEventListener("resize", function () {
+        if (this.innerWidth > mediaSize) {
+            resizeFix();
+        }
     });
-  
-  })();
+
+})();
+
+
+const typed = new Typed(".typed",{
+    strings:[
+        "HTML",
+        "CSS",
+        "JS",
+        "SASS",
+        "BOOSTRAP",
+        "PHP",
+        "MySql"
+    ],
+    stringsElements:"#cadenas-texto",
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    loopCount:false,
+    showCursor:false,
+    contentType:"html"
+
+
+    
+
+
+});
 
 
